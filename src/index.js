c61a0aa5631ca1b70bb25c690ea0b60863db9095
TatsianaSlapik <slapik.tatiana@gmail.com>
2020-10-22 12:46:03 +0300

    feat: added new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(expression, config) {
+    let pairsArray = config.map(subarray => subarray[0] + subarray[1]);
+
+    for (let j = 0; j < pairsArray.length * 2; j++) {
+        for (let i = 0; i < pairsArray.length; i++) {
+            while (expression.includes(pairsArray[i])) {
+                expression = expression.replace(pairsArray[i], "");
+            }
+        }
+    }
+
+    return expression.length == 0;
 }
